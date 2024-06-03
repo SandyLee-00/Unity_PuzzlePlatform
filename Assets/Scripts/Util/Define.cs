47570ee900f2862b518578e4d399559f6cc77a62
Sandy <syjy8809@gmail.com>
2024-06-03 17:08:43 +0900

    :tada: 필요한 에셋, 폴더구조 넣어서 프로젝트 시작하기

---
+++ Assets/Scripts/Util/Define.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Define ¸ðÀ½
+/// </summary>
+public class Define
+{
+    public enum Sound
+    {
+        Bgm = 0,
+        Effect,
+        Max,
+    }
+}
