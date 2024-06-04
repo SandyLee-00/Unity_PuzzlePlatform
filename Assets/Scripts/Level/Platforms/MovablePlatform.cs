a037b3357c97da3d774105b9de5fee6e99a5fe68
RCO8 <gurwo63@naver.com>
2024-06-04 11:18:58 +0900

    [FEAT] 이동 오브젝트 구현

---
+++ Assets/Scripts/Level/Platforms/MovablePlatform.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePlatform : Platform
+{
+    public MovablePlatformData moveData;
+    public float maxDistance;   //최대 갈 수 있는 거리
+    public float watingTime;    //다시 돌아가기 까지 대기하는 시간
+
+    float nowTime = 0;
+    Rigidbody rigidbody;
+    Vector3 startPos;
+
+    private bool isReverse;
+
+    void Start()
+    {
+        startPos = transform.position;  //원래 자리로 돌아오기 위한 위치
+    }
+
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform() //발판 움직임
+    {
+        int moveReverse = isReverse ? -1 : 1;
+
+        if (moveData.movableType == MovablePlatformType.Horizontal)
+        {
+            //x축 이동
+            //현재 위치에서 최대 거리보다 넘어가면
+            if (transform.position.x >= startPos.x + maxDistance && !isReverse)
+                WaitToReverse();
+            else if(transform.position.x <= startPos.x && isReverse)
+                WaitToReverse();
+            else
+            {
+                transform.Translate(Vector3.right * moveData.platformSpeed * moveReverse * Time.deltaTime);
+            }
+        }
+        else if (moveData.movableType == MovablePlatformType.Vertical)
+        {
+            //y축 이동
+            if (transform.position.y >= startPos.y + maxDistance && !isReverse)
+                WaitToReverse();
+            else if (transform.position.y <= startPos.y && isReverse)
+                WaitToReverse();
+            else
+            {
+                transform.Translate(Vector3.up * moveData.platformSpeed * moveReverse * Time.deltaTime);
+            }
+        }
+    }
+
+    void WaitToReverse()    //바꾸기 전까지 대기
+    {
+        nowTime += Time.deltaTime;
+        if (nowTime > watingTime)
+        {
+            nowTime = 0;
+            ChangeDirection();
+        }
+    }
+
+    void ChangeDirection()  //방향 전환
+    {
+        isReverse = !isReverse;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlayerMovement playerMovement))
+        {
+            playerMovement.transform.SetParent(gameObject.transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlayerMovement playerMovement))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
