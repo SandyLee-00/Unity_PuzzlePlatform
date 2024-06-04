115c2d207b10b2be49e856b18909ade44998045d
MothorMoth <mothormoth@gmail.com>
2024-06-04 14:00:34 +0900

    [FEAT] 기본 아이템 프리펩 추가

--- Assets/Scripts/Item/BuffItem.cs
+++ Assets/Scripts/Item/BuffItem.cs
@@ -3,13 +3,28 @@ using UnityEngine;
 public class BuffItem : ItemObject
 {
     public BuffItemData buffItemData;
+
     public override string GetPrompt()
     {
         return $"{buffItemData.itemName}\n{buffItemData.description}";
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerAttributeHandler attributeHandler))
+        {
+            switch (buffItemData.type)
+            {
+                case BuffItemType.Speed:
+                    attributeHandler.OnSpeedBuff(buffItemData);
+                    Destroy(gameObject);
+                    break;
 
-    //}
+                //case BuffItemType.Speed:
+                //    점프 처리
+                //    Destroy(gameObject);
+                //    break;
+            }
+        }
+    }
 }
--- Assets/Scripts/Item/ConsumableItem.cs
+++ Assets/Scripts/Item/ConsumableItem.cs
@@ -3,13 +3,28 @@ using UnityEngine;
 public class ConsumableItem : ItemObject
 {
     public ConsumableItemData consumableItemData;
+
     public override string GetPrompt()
     {
         return $"{consumableItemData.itemName}\n{consumableItemData.description}";
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerHealthMana playerStatus))
+        {
+            switch (consumableItemData.type)
+            {
+                case ConsumableItemType.Health:
+                    playerStatus.ChangeHP(consumableItemData.increaseValue);
+                    Destroy(gameObject);
+                    break;
 
-    //}
+                //case ConsumableItemType.Stamina:
+                //    playerStatus. 스테미나 처리
+                //    Destroy(gameObject);
+                //    break;
+            }
+        }
+    }
 }
