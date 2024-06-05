cb290673f0285e385e70a70ab0296bee77883a13
RCO8 <gurwo63@naver.com>
2024-06-05 11:28:50 +0900

    [STOP] TrapPlatform 인터페이스 구현중

--- Assets/Scripts/Level/Platforms/TrapPlatform.cs
+++ Assets/Scripts/Level/Platforms/TrapPlatform.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapPlatform : Platform
+public class TrapPlatform : Platform, IInspectable
 {
     public TrapPlatformData trapPlatform;
 
     //지속 데미지 릴레이 추가
 
+    public string GetPrompt()
+    {
+        return "";
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.TryGetComponent(out PlayerHealthMana playerState))
         {
-            Debug.Log(playerState.CurrentHP);
-            playerState.ChangeHP(-trapPlatform.damage);
-            Debug.Log(playerState.CurrentHP);
+            //체력 감소 (IDamagable이 추가되면 이동할 예정)
+            if (playerState.ChangeHP(-trapPlatform.damage))
+            {
+                Debug.Log(playerState.CurrentHP);
+            }
+            else
+            {
+                //gameOver
+            }
             //트랩마다 종류가 있을텐데 trapPlatform에서 타입을 확인하고 결정
         }
     }
