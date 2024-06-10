d2233b02b8a9419c2cd101cbcf7778c9bef0aa16
RCO8 <gurwo63@naver.com>
2024-06-10 14:13:38 +0900

    [FIX] TrapHole 코드 복원

--- Assets/Scripts/Level/Traps/TrapHole.cs
+++ Assets/Scripts/Level/Traps/TrapHole.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class TrapHole : MonoBehaviour
 {
     public Transform startPos;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(Define.PlayerTag);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.TryGetComponent(out PlayerHeartStamina player))
+        if (other.gameObject.TryGetComponent(out PlayerHeartStamina playerHeartStamina))
         {
-            player.ChangeHeart(-1);
-            Debug.Log("처음부터 시작");
-            player.transform.position = startPos.position; //시작 지점으로 추가
-            player.transform.rotation = startPos.rotation;
+            if (playerHeartStamina.ChangeHeart(-1))
+            {
+                Debug.Log("처음부터 시작");
+                StartCoroutine(GoToStartPosition(1f));
+            }
+            else
+            {
+                Debug.Log("게임오버");
+                //첫 화면으로 돌아가기
+            }
+
+            IEnumerator GoToStartPosition(float second)
+            {
+                yield return new WaitForSeconds(second);
+
+                player.transform.position = startPos.position; //시작 지점으로 추가
+                player.transform.rotation = startPos.rotation;
+            }
         }
     }
 }
